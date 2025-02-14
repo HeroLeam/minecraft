53e55ca8933a6e5f4dfbc4789cbe4e845fb3fb42
Thiago Leoni Amaral <thiagoleam@gmail.com>
2025-02-14 14:17:52 -0300

    Update

--- src/js/scrpit.js
+++ src/js/scrpit.js
@@ -4,25 +4,24 @@ function toggleMenu() {
 }
 
 async function checkServer() {
-  const ip = "herofield.com.br"; // Substitua pelo IP ou domínio do seu servidor
+  const ip = "herofield.com.br";
 
   try {
-    const response = await fetch(`https://api.mcsrvstat.us/2/${ip}`);
+    const response = await fetch(`https://api.mcstatus.io/v2/status/java/${ip}`);
     const data = await response.json();
 
     const statusDiv = document.getElementById("mcStatus");
     if (data.online) {
       statusDiv.innerText = `🟢 Online - ${data.players.online}/${data.players.max} jogadores!`;
+      statusDiv.className = "online";
     } else {
       statusDiv.innerText = "🔴 Servidor Offline";
+      statusDiv.className = "offline";
     }
   } catch (error) {
     document.getElementById("mcStatus").innerText = "Erro ao verificar o status";
   }
 }
 
-// Chama a função assim que o site carrega
 checkServer();
-
-// Atualiza o status automaticamente a cada 30 segundos
 setInterval(checkServer, 30000);
